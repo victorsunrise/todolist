12366e40939579040021b21a8fe6ca53fed5fc02
Victor Komlev <viktor-sunrise@outlook.com>
2017-12-02 19:21:35 +0400

    d.fkvbshdfkj

---
+++ src/components/Headers.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="headers">
+    <label class="headers__label"> Enter your todo: </label>
+    <input class="header__input" ref="todoinput" @keyup.enter="submit"></input>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  name: 'headers',
+  methods: {
+    submit(val) {
+      this.$store.dispatch('NEW_ITEM', { item: this.$refs.todoinput.value});
+      this.$refs.todoinput.value="";
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  .headers {
+    display: flex;
+    justify-content: center;
+      &__label {
+        padding-right: 50px;
+      }
+  }
+</style>
---
+++ src/views/All.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <headers></headers>
+  </div>
+</template>
+
+<script>
+import Item  from '../components/Item.vue';
+import Headers from '../components/Headers.vue'
+export default {
+  name: 'all',
+  components: {
+    Item,
+    Headers
+  },
+}
+
+</script>
+
+<style lang="less">
+</style>
