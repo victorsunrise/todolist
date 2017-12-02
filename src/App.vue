8f1afc006d632e1e9337b6160a279b50531b2dac
Victor Komlev <viktor-sunrise@outlook.com>
2017-12-02 14:48:52 +0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,55 @@
 <template>
   <div id="app">
-    <h1 class="ui dividing centered header">Vue.js Todo App</h1>
-    <div class='ui three column centered grid'>
-      <div class='column'>
-        <todo-list v-bind:todos="todos"></todo-list>
-        <create-todo v-on:create-todo="createTodo"></create-todo>
-      </div>
-    </div>
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
-import sweetalert from 'sweetalert';
-import TodoList from './components/TodoList';
-import CreateTodo from './components/CreateTodo';
-
 export default {
   name: 'app',
+
   components: {
-    TodoList,
-    CreateTodo,
+    //
   },
+
   data() {
     return {
-      todos: [{
-        title: 'Todo A',
-        project: 'Project A',
-        done: false,
-      }, {
-        title: 'Todo B',
-        project: 'Project B',
-        done: true,
-      }, {
-        title: 'Todo C',
-        project: 'Project C',
-        done: false,
-      }, {
-        title: 'Todo D',
-        project: 'Project D',
-        done: false,
-      }],
-    };
-  },
-  methods: {
-    createTodo(newTodo) {
-      this.todos.push(newTodo);
-      sweetalert('Success!', 'To-Do created!', 'success');
-    },
-  },
-};
+      msg: 'Vuex Examples'
+    }
+  }
+}
 </script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  h1 {
+    font-weight: normal;
+  }
+
+  h1>a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li>div {
+    margin-top: 10px;
+  }
+</style>
